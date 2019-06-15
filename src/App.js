9bad2ea6d13612846cd5f69eb3cd9ced734c0a01
Zeid Adabel <zedjam98@hotmail.com>
2019-06-16 00:40:20 +0200

    Login working, cookie name is token, use this on requests with token in header, router to be added...

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import axios from 'axios'
+import Cookies from 'universal-cookie';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      username: '',
+      password: ''
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleChange (event) {
+    this.setState( {[event.target.name]: event.target.value} )
+  }
+
+  handleClick (e) {
+    e.preventDefault();
+    console.log('Success!')
+    this.username = this.state.username;
+    this.password = this.state.password;
+
+    axios.post('https://localhost:8443/Login?username='+this.username+'&'+'password='+this.password,
+    {
+      headers: { 
+       "Access-Control-Allow-Origin": "*",
+       "Access-Control-Allow-Methods": "DELETE, POST, GET",
+       "Access-Control-Allow-Headers": "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"
+     }
+   })
+    .then(function(response){
+      console.log(response);
+      const cookies = new Cookies();
+      var token = response.data;
+      cookies.set('token', token, { path: '/' });
+      console.log(cookies.get('token')); 
+    })
+    .catch(err => {
+        // If there was a problem, we want to
+        console.log(err);
+        return err;
+      });
+  }
+
+  render () {
+    return (
+      <div className='login_container'>
+        <h2>Login to your account</h2>
+        <form id="login_window" className="login_form" name="login" onSubmit={this.handleClick}>
+          <input type="text" id="username" title="username"  name="username" 
+          onChange={event => this.handleChange(event)} placeholder="username" />
+          <input type="password" id="password" title="username" name="password" 
+          onChange={event => this.handleChange(event)}  placeholder="password" />
+          <button type="submit" className="login" name="login">Login</button>
+        </form>
+      </div>
+      /*<div className='button__container'>
+      <button className='button' onClick={this.handleClick}>
+      Click Me
+      </button>
+      </div>*/
+      )
+  }
 }
-
-export default App;
+export default App
