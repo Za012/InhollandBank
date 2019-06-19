1c94a177cb429a3a06681642c9bd1263f65d8cf9
Liestere <550178@student.inholland.nl>
2019-06-19 02:15:25 +0200

    Added design

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Login from './Login';
 import Search from './Search';
 import RegisterUser from './RegisterUser';
 import CustomerStatistics from './CustomerStatistics';
+import Home from './Home';
 import Profile from './components/Profile';
 import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
@@ -12,18 +13,18 @@ function App(){
     <Router>
       <div className="Login">
         <Switch>
-          <Route path="/" exact component={Home} />
           <Route path="/Login" component={Login} />
           <Route path="/Employee/Search" component={Search}/>
           <Route path="/Employee/RegisterUser"component={RegisterUser}/>
           <Route path="/Customer/Statistics" component={CustomerStatistics}/>
           <Route path="/profile" component={Profile}/>
+          <Route path="/Home" component={Home}/>
         </Switch>
       </div>
       </Router>
     )
 }
-  const Home = () => (
+  const yay = () => (
     <div>
       <h1>Home Page</h1>
     </div>
--- src/RegisterUser.js
+++ src/RegisterUser.js
@@ -1,14 +1,39 @@
 import React, { Component } from 'react'
-import './App.css'
 import axios from 'axios'
 import Cookies from 'universal-cookie';
-import AuthService from './AuthService';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
+import TopBar from "./HomeComponents/TopBar";
+import NavBar from "./RegisterComponents/NavBar";
+import MiddleBar from "./RegisterComponents/MiddleBar";
+
+const styles = {
+  Form:{
+    background : "#FDE4F2",
+    'border-style' : "Solid",
+    'border-color' : "#F9CEE7",
+    'border-width' : "1px",
+    position : "absolute",
+    padding: "20px",
+    left : "15%",
+    top : "330px",
+    width : "70%",
+    'border-radius' : "8px",
+    '-moz-border-radius' : "8px",
+    '-webkit-border-radius' : "8px"
+  },
+  Title:{
+    position : "absolute",
+  	top: "220px"
+  }
+};
 
 class RegisterUser extends Component {
 	constructor () {
 		super()
 		this.state = {
-			roles: null,
+			roles: "ROLE_CUSTOMER",
 			firstName: null,
 			lastName: null,
 			email: null,
@@ -26,6 +51,7 @@ class RegisterUser extends Component {
 	}
 
 	handleClick (e) {
+		this.state.dateCreated = new Date().toLocaleString()
 		var parent = this;
 		var url = 'https://localhost:8443/Employee/Users';
 		if(this.state.query != null && this.state.value != null){
@@ -47,29 +73,65 @@ class RegisterUser extends Component {
 
 	render () {
     return (
-      <div className='register_container'>
-		  <h2>Create a user!</h2>
-		  <form id="register_window" className="register_form" name="register" onSubmit={this.handleClick}>
-		      <input type="text" id="roles" title="roles"  name="roles" 
-		      onChange={event => this.handleChange(event)} placeholder="roles" />
-		      <input type="firstName" id="firstName" title="firstName"  name="firstName" 
-		      onChange={event => this.handleChange(event)} placeholder="firstName" />
-			  <input type="lastName" id="lastName" title="lastName"  name="lastName" 
-		      onChange={event => this.handleChange(event)} placeholder="lastName" />
-		      <input type="email" id="email" title="email"  name="email" 
-		      onChange={event => this.handleChange(event)} placeholder="email" />
-		      <input type="phone" id="phone" title="phone"  name="phone" 
-		      onChange={event => this.handleChange(event)} placeholder="phone" />
-		      <input type="username" id="username" title="username"  name="username" 
-		      onChange={event => this.handleChange(event)} placeholder="username" />
-		      <input type="password" id="password" title="password"  name="password" 
-		      onChange={event => this.handleChange(event)} placeholder="password" />
-		      <input type="date" id="birthday" title="birthday"  name="birthday" 
-		      onChange={event => this.handleChange(event)} placeholder="birthday" />
-		      <input type="date" id="dateCreated" title="dateCreated"  name="dateCreated" 
-		      onChange={event => this.handleChange(event)} placeholder="dateCreated" />
-		      <button type="submit" className="register" name="register_btn">Register!</button>
-		  </form>
+    	<div>
+	  <TopBar/>
+	  <MiddleBar/>
+	  <NavBar/>
+	  <h2 style={styles.Title}>Create a user!</h2>
+      <Form style={styles.Form} onSubmit={this.handleClick}>
+        <Form.Row>
+          <Form.Group as={Col} controlId="formGridRole">
+            <Form.Label>Role</Form.Label>
+            <Form.Control as="select" name="roles" onChange={event => this.handleChange(event)}>
+              <option>ROLE_CUSTOMER</option>
+              <option>ROLE_EMPLOYEE</option>
+            </Form.Control>
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="formGridFirstName">
+            <Form.Label>First name</Form.Label>
+            <Form.Control type="text" name="firstName" onChange={event => this.handleChange(event)} placeholder="Enter first name" />
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="formGridLastName">
+            <Form.Label>Last name</Form.Label>
+            <Form.Control type="text" name="lastName" onChange={event => this.handleChange(event)} placeholder="Enter last name" />
+          </Form.Group>
+        </Form.Row>
+
+        <Form.Row>
+          <Form.Group as={Col} controlId="formGridEmail">
+            <Form.Label>Email</Form.Label>
+            <Form.Control type="email" name="email" onChange={event => this.handleChange(event)} placeholder="Enter email" />
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="formGridPhone">
+            <Form.Label>Phone</Form.Label>
+            <Form.Control type="phone" name="phone" onChange={event => this.handleChange(event)} placeholder="Enter phone number" />
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="formGridBirth">
+            <Form.Label>Birthday</Form.Label>
+            <Form.Control type="date" name="birthday" onChange={event => this.handleChange(event)} placeholder="Enter birthday" />
+          </Form.Group>
+        </Form.Row>
+
+        <Form.Row>
+         <Form.Group as={Col} controlId="formGridUserame">
+            <Form.Label>Username</Form.Label>
+            <Form.Control type="text" name="username" onChange={event => this.handleChange(event)} placeholder="Enter username" />
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="formGridPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" name="password" onChange={event => this.handleChange(event)} placeholder="Password" />
+           </Form.Group>
+        </Form.Row>
+
+        <Button variant="success" type="submit">
+          Submit
+        </Button>
+      </Form>
       </div>
       )
   }
